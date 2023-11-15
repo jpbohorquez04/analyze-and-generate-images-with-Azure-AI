bbad98c13cba42375538d357e213c4ba308394a4
jpbohorquez04 <110682085+jpbohorquez04@users.noreply.github.com>
2023-11-15 16:01:06 +0000

    enviar imagen para que la API la lea

--- src/App.js
+++ src/App.js
@@ -5,9 +5,50 @@ import axios from 'axios';
 function App() {
   const [imageUrl, setImageUrl] = useState('');
   
-  const handleImageAnalysis = () => {
-    // Agrega aquí la lógica para analizar la imagen
-    console.log('Analizando imagen:', imageUrl);
+  const handleImageAnalysis = ({ imageUrl }) => {
+    const [result, setResult] = useState(null);
+
+    useEffect(() => {
+      const makeApiCall = async () => {
+        const url = "https://*.cognitiveservices.azure.com/computervision/imageanalysis:analyze";
+        const apiKey = "{subscription key}";
+  
+        const params = {
+          features: "{string}",
+          "model-name": "{string}",
+          language: "en",
+          "smartcrops-aspect-ratios": "{string}",
+          "gender-neutral-caption": "False",
+        };
+  
+        try {
+          const response = await axios.post(`${url}?api-version=2023-02-01-preview`, { url: imageUrl }, {
+            headers: {
+              "Content-Type": "application/json",
+              "Ocp-Apim-Subscription-Key": apiKey,
+            },
+          });
+  
+          // Actualizar el estado con la respuesta JSON
+          setResult(response.data);
+        } catch (error) {
+          console.error("Error:", error);
+          // Puedes manejar el error aquí si es necesario
+        }
+      };
+  
+      makeApiCall();
+    }, [imageUrl]);
+  
+    return (
+      <div>
+        {result && (
+          <pre>
+            {JSON.stringify(result, null, 2)}
+          </pre>
+        )}
+      </div>
+    );
   };
 
   const handleImageGeneration = () => {
@@ -27,56 +68,10 @@ function App() {
         onChange={(e) => setImageUrl(e.target.value)}
       />
 
-      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={() => handleImageAnalysis(imageUrl)}>Analizar Imagen</button>
       <button onClick={handleImageGeneration}>Generar Imagen</button>
 
       <AnalyzeImage imageUrl={imageUrl} />
     </div>
   );
 }
-
-const AnalyzeImage = ({ imageUrl }) => {
-  const [result, setResult] = useState(null);
-
-  useEffect(() => {
-    const makeApiCall = async () => {
-      const url = "https://*.cognitiveservices.azure.com/computervision/imageanalysis:analyze";
-      const apiKey = "{subscription key}";
-
-      const params = {
-        features: "{string}",
-        "model-name": "{string}",
-        language: "en",
-        "smartcrops-aspect-ratios": "{string}",
-        "gender-neutral-caption": "False",
-      };
-
-      try {
-        const response = await axios.post(`${url}?api-version=2023-02-01-preview`, { url: imageUrl }, {
-          headers: {
-            "Content-Type": "application/json",
-            "Ocp-Apim-Subscription-Key": apiKey,
-          },
-        });
-
-        // Actualizar el estado con la respuesta JSON
-        setResult(response.data);
-      } catch (error) {
-        console.error("Error:", error);
-        // Puedes manejar el error aquí si es necesario
-      }
-    };
-
-    makeApiCall();
-  }, [imageUrl]);
-
-  return (
-    <div>
-      {result && (
-        <pre>
-          {JSON.stringify(result, null, 2)}
-        </pre>
-      )}
-    </div>
-  );
-};
